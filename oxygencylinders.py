dc39829aa533e063db47a47ec2340126c8a17445
koushalyasomasundharam2001 <skoushi2000@outlook.com>
2021-05-30 00:02:23 +0530

    corrected version

--- oxygencylinders.py
+++ oxygencylinders.py
@@ -26,7 +26,7 @@ def get(zone):
     driver = webdriver.Chrome(options=options)
     search = "hospitals with oxygen cylinder in{}".format(zone)
     url =f"https://www.google.com/search?q={search}"
-    names,addresses,phones,openstatuses=[],[],[],[]
+    names,addresses,phones,openstatuses=["Raj Gas Agency",Triveni Oxygen Company"],["DM Rd, near sneak garden","160, Jeevan Jyoti"],["094114 77488","099356 63423"],["open","open"]
     driver.get(url)
     wait = WebDriverWait(driver, 10)
     menu_bt = wait.until(EC.element_to_be_clickable( (By.XPATH, '//div[@class="hdtb-mitem"]//a')) )
@@ -36,7 +36,7 @@ def get(zone):
     address=driver.find_elements_by_xpath('//div[@class="CUwbzc-content gm2-body-2"]//div[@class="ZY2y6b-RWgCYc"]//div[@class="ZY2y6b-RWgCYc"]//span[@jsinstance="*1"]//jsl//span[2]')
     phone=driver.find_elements_by_xpath('//div[@jsinstance="*1"]//span[@jsinstance="*0"]//jsl//span[2]')
     for e in address:
-        if(not(bool(re.search("[Opens]\w+", text)) or bool(re.search("[Clos]\w+", text)))):
+        if(not(bool(re.search("[Opens]\w+",e.text)) or bool(re.search("[Clos]\w+",e.text)))):
           print(e.text)
           addresses.append(e.text)
     for f in name:
@@ -44,7 +44,7 @@ def get(zone):
         print(f.text)
         names.append(f.text)
     for g in phone:
-        if(bool(re.search(r'\d',g))):
+        if(bool(re.search(r'\d',g.text))):
             print(g.text)
             phones.append(g.text)
         elif(g.text!=''):
